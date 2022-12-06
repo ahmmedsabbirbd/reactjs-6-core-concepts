2697e9b818a65d5dfa57a2ca51316120d51bf88e
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-12-06 18:48:39 +0600

    fake json data genaretor and fectch use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { multiple, sum } from './utility/fuction';
+import { useEffect, useState } from 'react';
 
 function App() {
   const a = 5;
@@ -8,10 +9,21 @@ function App() {
   const total = sum(a, b);
   const totalMultiple = multiple(a, b);
 
+  const [user, setUser] = useState([]);
+
+  useEffect(()=> {
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => setUser(data));
+  }, []);
+
+  console.log(user);
+
   return (
     <div className="App">
         <h2>you sum: {total}</h2>
         <h2>you multiple: {totalMultiple}</h2>
+        {}
     </div>
   );
 }
