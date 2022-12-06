24a4eef19af7f6362e03f70e1adc4e92af918a48
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-12-06 19:06:40 +0600

    Create Event handler with parameters

--- src/App.js
+++ src/App.js
@@ -9,23 +9,38 @@ function App() {
   const total = sum(a, b);
   const totalMultiple = multiple(a, b);
 
-  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(()=> {
     fetch('data.json')
     .then(res => res.json())
-    .then(data => setUser(data));
+    .then(data => setUsers(data));
   }, []);
 
-  console.log(user);
+  console.log(users);
 
   return (
     <div className="App">
         <h2>you sum: {total}</h2>
         <h2>you multiple: {totalMultiple}</h2>
-        {}
+        {
+          users.map((user) => <User id={user.id}></User>)
+        }
     </div>
   );
 }
+
+const add =(id) => {
+    console.log(id);
+}
+ 
+const User = (props) => {
+  return (
+    <div>
+      <h2>{props.id}</h2>
+      <button onClick={()=> add(props.id)}>add</button>
+    </div>
+  );
+}; 
 
 export default App;
