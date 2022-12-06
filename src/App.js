867c26f5374c2fea4e066721ce6691ece6e551f4
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-12-06 18:31:54 +0600

    import modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { multiple, sum } from './utility/fuction';
 
 function App() {
+  const a = 5;
+  const b = 3;
+  const total = sum(a, b);
+  const totalMultiple = multiple(a, b);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>you sum: {total}</h2>
+        <h2>you multiple: {totalMultiple}</h2>
     </div>
   );
 }
