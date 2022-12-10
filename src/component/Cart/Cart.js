852c9ce80e3427ca946e218404b5e1f4baa25583
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-12-10 12:48:26 +0600

    49-3

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
+    let total = 0;
+    let shipping = 0;
+    console.log(cart);
+    for(const singleeCart of cart) {
+        total += singleeCart.price;
+        shipping += singleeCart.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2) + '';
 
     return ( 
         <div className="summary">
@@ -9,6 +17,12 @@ const Cart = ({ cart }) => {
                 
             <ul>
                 <li>Selected Items: {cart.length}</li>
+                <li>Total Price: ${total}</li>
+                <li>Total Shipping Charge: ${shipping}</li>
+                <li>Tax: ${tax}</li>
+                <li>Grand Total: $1559</li>
+                <li><button>Clear Cart</button></li>
+                <li><button>Review Order</button></li>
             </ul>
         </div>
     );
